ccff95ae1423649d818600a872aa6705f0c57a1a
AshedFox <a.mahn@mail.ru>
2025-11-03 13:50:53 +0300

    fix(api): add @Type decorator on user to transform user in auth response

--- apps/api/src/auth/dto/auth-response.dto.ts
+++ apps/api/src/auth/dto/auth-response.dto.ts
@@ -1,8 +1,12 @@
+import { Type } from 'class-transformer';
+
 import { UserDto } from '@/user/dto/user.dto';
 
 export class AuthResponseDto {
   accessToken!: string;
   tokenType!: 'Bearer';
   expiresIn!: number;
+
+  @Type(() => UserDto)
   user!: UserDto;
 }

81072873e915e8cd0afa6c727a3e4ab407b1e00b
AshedFox <a.mahn@mail.ru>
2025-11-03 13:33:50 +0300

    refactor(api): add barrel files to simplify imports from search module

---
+++ apps/api/src/common/search/index.ts
@@ -0,0 +1,5 @@
+export * from './dto';
+export * from './helpers';
+export * from './transformers';
+export * from './types';
+export * from './validators';
---
+++ apps/api/src/common/search/types/index.ts
@@ -0,0 +1,4 @@
+export * from './filter.types';
+export * from './pagination.types';
+export * from './search.types';
+export * from './sort.types';

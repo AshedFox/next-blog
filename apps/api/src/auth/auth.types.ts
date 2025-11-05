54c7e69a2cb75ab32b95c917a7d7305d8f304f33
AshedFox <a.mahn@mail.ru>
2025-11-05 16:53:42 +0300

    feat(api)!: change auth response dto, add refresh token, change expiresIn to accessTokenExpiresAt

--- apps/api/src/auth/auth.types.ts
+++ apps/api/src/auth/auth.types.ts
@@ -1,9 +1,3 @@
-import { AuthResponseDto } from './dto/auth-response.dto';
-
-export type AuthResult = AuthResponseDto & {
-  refreshToken: string;
-};
-
 export type TokenPayload = {
   sub: string;
 };

fe5b6a0327108dd0726e03ab6e0987f247ede0f0
AshedFox <a.mahn@mail.ru>
2025-11-07 11:35:45 +0300

    feat(web): implement sign-up page

---
+++ apps/web/src/app/(guest)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from 'next';
+import React from 'react';
+
+import { SignUpForm } from '@/features/auth/client';
+
+export const metadata: Metadata = {
+  title: 'Sign Up',
+};
+
+export default async function Page() {
+  'use cache';
+
+  return (
+    <div className="grow flex items-center justify-center from-primary/20 to-card bg-linear-115">
+      <div className="w-full max-w-md">
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}

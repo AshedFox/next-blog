cdce0cc5ca45702c67923de633d303df4d1526c8
AshedFox <a.mahn@mail.ru>
2025-11-07 11:30:24 +0300

    feat(web): implement LoginForm

---
+++ apps/web/src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, buttonVariants } from '@workspace/ui/components/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@workspace/ui/components/card';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@workspace/ui/components/field';
+import { Input } from '@workspace/ui/components/input';
+import { cn } from '@workspace/ui/lib/utils';
+import Link from 'next/link';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+
+import Spinner from '@/shared/components/Spinner';
+
+import { login } from '../actions/login';
+import { LoginInput } from '../types';
+import { loginSchema } from '../validation/login-schema';
+
+export const LoginForm = () => {
+  const form = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (data: LoginInput) => {
+    const { message } = await login(data);
+
+    if (message) {
+      toast.error('Login failed', {
+        description: message,
+      });
+    }
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-3xl font-bold">Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form id="login-form" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+              control={form.control}
+              name="email"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Email</FieldLabel>
+                  <Input
+                    {...field}
+                    type="email"
+                    placeholder="Your email..."
+                    autoComplete="email"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="password"
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel>Password</FieldLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    autoComplete="current-password"
+                    placeholder="Your password..."
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Field orientation="horizontal">
+          <Button type="submit" form="login-form">
+            {form.formState.isSubmitting ? (
+              <>
+                <Spinner /> Logging In...
+              </>
+            ) : (
+              'Login'
+            )}
+          </Button>
+          <Link
+            className={cn(
+              buttonVariants({ variant: 'link', size: 'sm' }),
+              'text-xs'
+            )}
+            href="/sign-up"
+          >
+            Don&apos;t have an account?
+          </Link>
+        </Field>
+      </CardFooter>
+    </Card>
+  );
+};

942b7135a12d71a1d495115f7fb0d97c743dc22a
AshedFox <a.mahn@mail.ru>
2025-11-20 16:58:05 +0300

    refactor(api): remove redundant common sub-modules

--- apps/api/src/common/search/types/sort.types.ts
+++
@@ -1,8 +0,0 @@
-export const SortDirection = { ASC: 'asc', DESC: 'desc' } as const;
-
-export type SortDirection = (typeof SortDirection)[keyof typeof SortDirection];
-
-export type SortField<T> = {
-  field: keyof T;
-  direction: SortDirection;
-};
--- apps/api/src/common/search/validators/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './filter.validator';
-export * from './relations.validator';
-export * from './search.validator';
-export * from './sort.validator';

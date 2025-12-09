50ae6af59433e03c7a91ee17c9346639add43cb0
AshedFox <a.mahn@mail.ru>
2025-12-09 17:10:11 +0300

    feat(contracts): add enum for article list style

--- packages/contracts/src/article/enums/index.ts
+++ packages/contracts/src/article/enums/index.ts
@@ -1,5 +1,6 @@
 export * from './article-block-type';
 export * from './article-include';
+export * from './article-list-style';
 export * from './article-status';
 export * from './code-language';
 export * from './video-provider';

b2c76fef6f716f26ab9349c23944582f6bcc2ab1
AshedFox <a.mahn@mail.ru>
2025-12-16 14:26:04 +0300

    feat(contracts): add id to article blocks

--- packages/contracts/src/article/schemas/article-blocks-schemas.ts
+++ packages/contracts/src/article/schemas/article-blocks-schemas.ts
@@ -3,48 +3,52 @@ import z from 'zod';
 import { ArticleBlockType, ArticleListStyle, VideoProvider } from '../enums';
 import { articleSegmentSchema } from './article-segments-schemas';
 
-export const articleParagraphBlockSchema = z.object({
+export const baseArticleBlockSchema = z.object({
+  id: z.uuid(),
+});
+
+export const articleParagraphBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.PARAGRAPH),
   content: z.array(articleSegmentSchema),
 });
 
-export const articleImageBlockSchema = z.object({
+export const articleImageBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.IMAGE),
   fileId: z.uuid(),
   url: z.url(),
   alt: z.string().optional(),
 });
 
-export const articleVideoBlockSchema = z.object({
+export const articleVideoBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.VIDEO),
   provider: z.enum(VideoProvider),
   videoId: z.string(),
   embedUrl: z.url(),
 });
 
-export const articleCodeBlockSchema = z.object({
+export const articleCodeBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.CODE),
   content: z.string(),
   language: z.string().optional(),
 });
 
-export const articleQuoteBlockSchema = z.object({
+export const articleQuoteBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.QUOTE),
   content: z.array(articleSegmentSchema),
   author: z.string().optional(),
 });
 
-export const articleHeadingBlockSchema = z.object({
+export const articleHeadingBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.HEADING),
   content: z.array(articleSegmentSchema),
   level: z.number(),
 });
 
-export const articleDividerBlockSchema = z.object({
+export const articleDividerBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.DIVIDER),
 });
 
-export const articleListBlockSchema = z.object({
+export const articleListBlockSchema = baseArticleBlockSchema.extend({
   type: z.literal(ArticleBlockType.LIST),
   style: z.enum(ArticleListStyle),
   items: z.array(articleParagraphBlockSchema),

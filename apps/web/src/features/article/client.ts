ad44eff0f6ececd407a5e77da12e526cd95bf4d0
AshedFox <a.mahn@mail.ru>
2025-12-17 18:52:08 +0300

    feat(web): add server and client export to article feature module

---
+++ apps/web/src/features/article/client.ts
@@ -0,0 +1,4 @@
+export * from './actions/create-article';
+export * from './components/CreateArticleForm';
+export * from './constants';
+export * from './utils';
---
+++ apps/web/src/features/article/server.ts
@@ -0,0 +1,5 @@
+import 'server-only';
+
+export * from './actions/create-article';
+export * from './api/server';
+export * from './utils';

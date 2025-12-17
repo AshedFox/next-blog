4c7fb3a1a3cb3f22136c0929001429e336c0ffc1
AshedFox <a.mahn@mail.ru>
2025-12-17 15:57:06 +0300

    feat(web): configure tiptap editor extensions

---
+++ apps/web/src/features/editor/lib/extensions-config.ts
@@ -0,0 +1,59 @@
+import Image from '@tiptap/extension-image';
+import ListItem from '@tiptap/extension-list-item';
+import { UniqueID } from '@tiptap/extension-unique-id';
+import Youtube from '@tiptap/extension-youtube';
+import { Extensions } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
+import { CustomCodeBlock } from '../components/CustomCodeBlock';
+import { CustomQuote } from '../components/CustomQuote';
+
+export const extensions: Extensions = [
+  StarterKit.configure({
+    codeBlock: false,
+    blockquote: false,
+    heading: { levels: [2, 3, 4] },
+    listItem: false,
+  }),
+  ListItem.extend({ content: 'paragraph' }),
+  CustomCodeBlock,
+  CustomQuote,
+  Image.extend({
+    addAttributes() {
+      return {
+        ...this.parent?.(),
+        fileId: {
+          default: null,
+          parseHTML: (element) => element.getAttribute('data-file-id'),
+          renderHTML: (attributes) => {
+            if (!attributes.fileId) {
+              return {};
+            }
+            return { 'data-file-id': attributes.fileId };
+          },
+        },
+      };
+    },
+  }).configure({
+    allowBase64: true,
+    HTMLAttributes: { class: 'rounded-lg' },
+  }),
+  Youtube.configure({
+    HTMLAttributes: { class: 'w-full aspect-video rounded-lg' },
+  }),
+  UniqueID.configure({
+    attributeName: 'id',
+    types: [
+      'heading',
+      'paragraph',
+      'image',
+      'youtube',
+      'codeBlock',
+      'quote',
+      'bulletList',
+      'orderedList',
+      'listItem',
+      'horizontalRule',
+    ],
+  }),
+];

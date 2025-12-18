a5703373f56355fc60701691ee97c436475cce8f
AshedFox <a.mahn@mail.ru>
2025-12-18 16:09:42 +0300

    feat(web): update article feature module exports

--- apps/web/src/features/article/client.ts
+++ apps/web/src/features/article/client.ts
@@ -1,4 +1,7 @@
 export * from './actions/create-article';
+export * from './actions/edit-article';
 export * from './components/CreateArticleForm';
+export * from './components/EditArticle';
+export * from './components/EditArticleForm';
 export * from './constants';
 export * from './utils';
--- apps/web/src/features/article/server.ts
+++ apps/web/src/features/article/server.ts
@@ -1,5 +1,6 @@
 import 'server-only';
 
 export * from './actions/create-article';
+export * from './actions/edit-article';
 export * from './api/server';
 export * from './utils';

16f3036df006b3f1f4eb2bb4637e559c0a6a19cf
AshedFox <a.mahn@mail.ru>
2025-12-19 16:19:26 +0300

    feat(web): add article component

--- apps/web/src/features/article/client.ts
+++ apps/web/src/features/article/client.ts
@@ -1,5 +1,6 @@
 export * from './actions/create-article';
 export * from './actions/edit-article';
+export * from './components/article';
 export * from './components/CreateArticleForm';
 export * from './components/EditArticle';
 export * from './components/EditArticleForm';

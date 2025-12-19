388f7705ce5b35490d61a9dc4bf108a4d6e61905
AshedFox <a.mahn@mail.ru>
2025-12-19 17:02:01 +0300

    feat(web): implement hero section

---
+++ apps/web/src/shared/components/hero/LoginOrWriteButton.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@workspace/ui/components/button';
+import { ArrowRight, UserIcon } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+
+import { getMe } from '@/features/user/server';
+
+const LoginOrWriteButton = async () => {
+  const user = await getMe();
+
+  if (!user) {
+    return (
+      <Button size="xl" asChild>
+        <Link href="/login">
+          Join Us <UserIcon />
+        </Link>
+      </Button>
+    );
+  }
+
+  return (
+    <Button size="xl" asChild>
+      <Link href="/articles/new">
+        Start writing <ArrowRight />
+      </Link>
+    </Button>
+  );
+};
+
+export default LoginOrWriteButton;

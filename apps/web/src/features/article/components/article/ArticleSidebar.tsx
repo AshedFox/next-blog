f89bacc7260021197f2ea7b4c74149a055bebe3a
AshedFox <a.mahn@mail.ru>
2025-12-19 16:06:44 +0300

    feat(web): add article sidebar component

---
+++ apps/web/src/features/article/components/article/ArticleSidebar.tsx
@@ -0,0 +1,55 @@
+import { Button } from '@workspace/ui/components/button';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@workspace/ui/components/card';
+import { format } from 'date-fns';
+import Link from 'next/link';
+import React from 'react';
+
+import { UserAvatar } from '@/features/user/client';
+
+import { getArticle } from '../../server';
+
+type Props = {
+  slugOrId: string;
+};
+
+const ArticleSidebar = async ({ slugOrId }: Props) => {
+  const result = await getArticle(slugOrId);
+
+  if (result.error) {
+    return null;
+  }
+
+  const author = result.data.author;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>About author</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-row items-center gap-2">
+        <UserAvatar user={author} className="size-20" />
+        <div>
+          <div className="text-lg">{author.name}</div>
+          <Button
+            className="p-0 text-sm text-primary-highlight h-fit"
+            size="sm"
+            variant="link"
+            asChild
+          >
+            <Link href={`/users/${author.username}`}>@{author.username}</Link>
+          </Button>
+          <div className="text-xs text-muted-foreground">
+            Since {format(author.createdAt, 'PPP')}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ArticleSidebar;

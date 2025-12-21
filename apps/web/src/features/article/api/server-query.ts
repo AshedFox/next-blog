c477ca9a5bab67fdfeee2249fff5b99e0284077e
AshedFox <a.mahn@mail.ru>
2025-12-21 15:43:10 +0300

    feat(web): implement articles search query

--- apps/web/src/features/article/api/server-query.ts
+++ apps/web/src/features/article/api/server-query.ts
@@ -1,9 +1,9 @@
-import { ArticleWithRelationsDto } from '@workspace/contracts';
+import { ArticleSearch, ArticleWithRelationsDto } from '@workspace/contracts';
 import { cacheTag } from 'next/cache';
 
 import { ApiFetchResult } from '@/lib/api/types';
 
-import { fetchArticle } from './server-transport';
+import { fetchArticle, fetchArticleList } from './server-transport';
 
 export async function getArticle(
   slugOrId: string
@@ -12,4 +12,11 @@ export async function getArticle(
   cacheTag(`articles-${slugOrId}`);
 
   return fetchArticle(slugOrId, ['author']);
+}
+
+export async function searchArticles(query: ArticleSearch) {
+  'use cache';
+  cacheTag('articles');
+
+  return fetchArticleList(query);
 }

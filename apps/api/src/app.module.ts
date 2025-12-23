c060e9f31bcfea720366a3fe6387340062ffdd8c
AshedFox <a.mahn@mail.ru>
2025-12-23 17:30:51 +0300

    feat(api): add http excpetion filter to log zod serialization errors

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
 
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
+import { HttpExceptionFilter } from './common/http/http-exception-filter';
 import { FileModule } from './file/file.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { RedisModule } from './redis/redis.module';
@@ -26,6 +27,7 @@ import { UserModule } from './user/user.module';
   providers: [
     { provide: APP_INTERCEPTOR, useClass: ZodSerializerInterceptor },
     { provide: APP_PIPE, useClass: ZodValidationPipe },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
   ],
 })
 export class AppModule {}

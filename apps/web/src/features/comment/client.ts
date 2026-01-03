392be2a4399b21648cf10552e13fa1ab48e229f4
AshedFox <a.mahn@mail.ru>
2026-01-03 20:21:35 +0300

    feat(web): implement comment card component

--- apps/web/src/features/comment/client.ts
+++ apps/web/src/features/comment/client.ts
@@ -2,4 +2,5 @@ export * from './actions/create-comment';
 export * from './actions/delete-comment';
 export * from './actions/edit-comment';
 export * from './api/client';
+export * from './components/CommentCard';
 export * from './hooks';

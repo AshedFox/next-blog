d6b4ad64cd0d325a4793a9a2e69ca36bca8582b4
AshedFox <a.mahn@mail.ru>
2026-01-05 14:21:32 +0300

    refactor(api): move article comments search to comment module

--- apps/api/src/comment/comment.module.ts
+++ apps/api/src/comment/comment.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { ArticleCommentController } from './article-comment.controller';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 
 @Module({
-  controllers: [CommentController],
+  controllers: [CommentController, ArticleCommentController],
   providers: [CommentService],
   exports: [CommentService],
 })

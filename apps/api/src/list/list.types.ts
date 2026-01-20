940f97c1fce5e881d984e8ac0e27d0935ae61448
AshedFox <a.mahn@mail.ru>
2026-01-20 13:12:03 +0300

    feat(api): add ListInclusionState type

--- apps/api/src/list/list.types.ts
+++ apps/api/src/list/list.types.ts
@@ -1,5 +1,14 @@
-import { CreateListDto } from '@workspace/contracts';
+import { CreateListDto, ListInclusionStateDto } from '@workspace/contracts';
+
+import { List } from '@/prisma/generated/client';
 
 export type CreateListInput = CreateListDto & {
   userId: string;
+};
+
+export type ListInclusionState = Pick<
+  ListInclusionStateDto,
+  'isFavorite' | 'isReadLater'
+> & {
+  includedInCustomLists: List[];
 };

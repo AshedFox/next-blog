3eab67a013f2ef9af7b4bfb01a23da7bc50c7577
AshedFox <a.mahn@mail.ru>
2026-02-23 22:17:56 +0300

    chore: update next config (add images config and standalone output)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const storageUrl = new URL(
+  process.env.STORAGE_PUBLIC_URL || 'http://localhost:9000'
+);
+
 const nextConfig = {
   cacheComponents: true,
-  transpilePackages: ['@workspace/ui'],
+  transpilePackages: ['@workspace/ui', '@workspace/contracts'],
+  images: {
+    unoptimized: storageUrl.hostname === 'localhost',
+    remotePatterns: [
+      {
+        protocol: storageUrl.protocol.replace(':', ''),
+        hostname: storageUrl.hostname,
+        port: storageUrl.port,
+        pathname: '/**',
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;

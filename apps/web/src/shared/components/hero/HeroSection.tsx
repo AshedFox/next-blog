b2c623934a20cec43851d7ea9fa58ab3d393e8e3
AshedFox <a.mahn@mail.ru>
2026-02-24 14:07:57 +0300

    refactor(web): improve hero section responsiveness

--- apps/web/src/shared/components/hero/HeroSection.tsx
+++ apps/web/src/shared/components/hero/HeroSection.tsx
@@ -8,29 +8,29 @@ import LoginOrWriteButton from './LoginOrWriteButton';
 
 export const HeroSection = () => {
   return (
-    <section className="relative bg-hero text-hero-foreground shadow-2xl rounded-b-[4rem] overflow-hidden py-20 px-4">
+    <section className="relative bg-hero text-hero-foreground shadow-2xl rounded-b-4xl md:rounded-b-[4rem] overflow-hidden py-10 lg:py-14 xl:py-18 2xl:py-20 min-h-[80svh] px-4 flex items-center justify-center @container">
       <div className="absolute top-0 left-0 size-full overflow-hidden z-0 pointer-events-none">
         <div className="absolute -top-66 -right-66 size-132 rounded-full blur-3xl bg-primary/70" />
         <div className="absolute -bottom-16 -left-24 size-92 rounded-full blur-3xl bg-primary/70" />
       </div>
-      <div className="max-w-4xl mx-auto relative flex flex-col items-center gap-12">
+      <div className="max-w-6xl relative flex flex-col items-center gap-6 @sm:gap-8 @lg:gap-10">
         <Badge size="lg">
           <SparklesIcon />
           Where words matter
         </Badge>
-        <div className="flex flex-col items-center gap-10 text-center">
-          <h1 className="text-6xl xl:text-8xl font-bold leading-tight">
+        <div className="z-10 flex flex-col items-center gap-4 @sm:gap-6 @lg:gap-10 text-center">
+          <h1 className="text-5xl @xs:text-6xl @md:text-7xl @lg:text-8xl @xl:text-9xl font-black leading-[1.2]">
             Ideas that
             <br />
             <span className="text-primary-highlight">shape the future</span>
           </h1>
-          <p className="text-xl xl:text-2xl text-hero-foreground/70">
+          <p className="text-sm @sm:text-base @lg:text-lg text-hero-foreground/70">
             A home for bold thinking and deep stories. Craft your masterpiece
             with our distraction-free editor or discover perspectives that
             inspire.
           </p>
         </div>
-        <div className="grid md:grid-cols-2 gap-2 md:gap-4 justify-center">
+        <div className="grid grid-cols-2 gap-2 @md:gap-4 justify-center">
           <Button variant="secondary" size="xl" asChild>
             <Link href="/articles">
               <CompassIcon /> Discover

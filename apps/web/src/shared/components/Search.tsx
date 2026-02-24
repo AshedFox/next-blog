5623b2c9e0976145ef7caba84ac96f525eba575f
AshedFox <a.mahn@mail.ru>
2026-02-24 14:03:29 +0300

    refactor(web): improve search component with ButtonGroup

--- apps/web/src/shared/components/Search.tsx
+++ apps/web/src/shared/components/Search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@workspace/ui/components/button';
+import { ButtonGroup } from '@workspace/ui/components/button-group';
 import {
   InputGroup,
   InputGroupAddon,
@@ -20,7 +21,7 @@ const Search = () => {
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (search === searchParams.get('search')) {
+    if (search == searchParams.get('search')) {
       return;
     }
 
@@ -38,22 +39,28 @@ const Search = () => {
 
   return (
     <form className="flex items-center gap-2 w-full" onSubmit={handleSearch}>
-      <InputGroup>
-        <InputGroupInput
-          name="search"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <InputGroupAddon>
+      <ButtonGroup className="grow">
+        <InputGroup>
+          <InputGroupInput
+            name="search"
+            value={search}
+            placeholder="Search..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <InputGroupAddon align="inline-end">
+            <InputGroupButton
+              size="icon-xs"
+              disabled={search.length === 0}
+              onClick={() => setSearch('')}
+            >
+              <XIcon />
+            </InputGroupButton>
+          </InputGroupAddon>
+        </InputGroup>
+        <Button variant="outline" aria-label="search">
           <SearchIcon />
-        </InputGroupAddon>
-        <InputGroupAddon align="inline-end">
-          <InputGroupButton size="icon-xs" onClick={() => setSearch('')}>
-            <XIcon />
-          </InputGroupButton>
-        </InputGroupAddon>
-      </InputGroup>
-      <Button>Search</Button>
+        </Button>
+      </ButtonGroup>
     </form>
   );
 };

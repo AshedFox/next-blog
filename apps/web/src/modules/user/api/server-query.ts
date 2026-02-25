28c9121445150678b98571620094a8e6b51a8558
AshedFox <a.mahn@mail.ru>
2026-02-25 20:48:30 +0300

    feat(web): add getUser query

--- apps/web/src/modules/user/api/server-query.ts
+++ apps/web/src/modules/user/api/server-query.ts
@@ -1,12 +1,12 @@
 import 'server-only';
 
-import { UserDto } from '@workspace/contracts';
+import { UserDto, UserGetOneDto, UserInclude } from '@workspace/contracts';
 import { cacheTag } from 'next/cache';
 import { cookies } from 'next/headers';
 
 import { getAccessToken } from '@/modules/auth/server';
 
-import { fetchMe } from './server-transport';
+import { fetchMe, fetchUser } from './server-transport';
 
 export async function getMe() {
   const token = await getAccessToken(await cookies());
@@ -19,6 +19,25 @@ async function _getMeCached(token: string): Promise<UserDto | undefined> {
 
   if (data) {
     cacheTag(`users-${data.id}`);
+  }
+
+  return data;
+}
+
+export async function getUser<T extends readonly UserInclude[]>(
+  idOrUsername: string,
+  query?: Partial<UserGetOneDto> & { include?: T }
+) {
+  'use cache';
+  const { data, error } = await fetchUser(idOrUsername, query);
+
+  if (error) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+
+  if (data) {
+    cacheTag(`users-${data.id}`);
+    cacheTag(`users-${data.username}`);
   }
 
   return data;
--- apps/web/src/modules/user/api/server-transport.ts
+++ apps/web/src/modules/user/api/server-transport.ts
@@ -1,9 +1,28 @@
 import 'server-only';
 
-import { UserDto } from '@workspace/contracts';
+import {
+  UserDto,
+  UserGetOneDto,
+  userGetOneSchema,
+  UserInclude,
+  UserWithRelationsDto,
+} from '@workspace/contracts';
 
 import { serverApi } from '@/lib/api/server';
+import { createApiSearchParams } from '@/lib/search-params';
 
 export function fetchMe(token?: string) {
   return serverApi.get<UserDto>(`/api/users/me`, token);
+}
+
+export function fetchUser(
+  idOrUsername: string,
+  query?: Partial<UserGetOneDto>
+) {
+  const search = userGetOneSchema.parse(query || {});
+  const searchParams = createApiSearchParams(search);
+
+  return serverApi.get<UserWithRelationsDto<UserInclude[]>>(
+    `/api/users/${idOrUsername}?${searchParams.toString()}`
+  );
 }

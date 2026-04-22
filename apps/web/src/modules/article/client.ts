bf38346e9fb6e1a7589b584228925a80ba97684b
AshedFox <a.mahn@mail.ru>
2026-04-22 19:03:45 +0300

    feat(web): add admin catalog for moderation
    Add separate AdminArticleCard and /admin/articles page

--- apps/web/src/modules/article/client.ts
+++ apps/web/src/modules/article/client.ts
@@ -1,5 +1,6 @@
 export * from './actions/create-article';
 export * from './actions/edit-article';
+export * from './components/AdminArticleCard';
 export * from './components/ArticleCard';
 export * from './components/CreateArticleForm';
 export * from './components/EditArticleForm';

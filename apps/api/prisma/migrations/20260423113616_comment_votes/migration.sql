c5856e4814451f7f5ee0055485d8d2be9f0cae82
AshedFox <a.mahn@mail.ru>
2026-04-27 16:11:35 +0300

    feat(api): add comment vote entity to database

---
+++ apps/api/prisma/migrations/20260423113616_comment_votes/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "CommentVote" (
+    "id" UUID NOT NULL,
+    "value" SMALLINT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" UUID NOT NULL,
+    "commentId" UUID NOT NULL,
+
+    CONSTRAINT "CommentVote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CommentVote_userId_commentId_key" ON "CommentVote"("userId", "commentId");
+
+-- AddForeignKey
+ALTER TABLE "CommentVote" ADD CONSTRAINT "CommentVote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentVote" ADD CONSTRAINT "CommentVote_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

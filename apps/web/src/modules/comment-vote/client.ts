64a2a630138d5f7ba2a509967ef10935fea8ff01
AshedFox <a.mahn@mail.ru>
2026-04-27 16:23:02 +0300

    chore(web): add client and server re-exports

---
+++ apps/web/src/modules/comment-vote/client.ts
@@ -0,0 +1,6 @@
+export * from './actions/deleteCommentVote';
+export * from './actions/downvoteComment';
+export * from './actions/upvoteComment';
+export * from './api/client';
+export * from './components/CommentVotes';
+export * from './components/CommentVotesSkeleton';
